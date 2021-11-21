75dfb65f170671eb0e49940d01ac90033259e931
tommytz <ttardizuch@gmail.com>
2021-11-22 11:41:58 +1300

    Added delete button

--- buttonScripts.js
+++ buttonScripts.js
@@ -9,16 +9,29 @@ function userNote() {
 
 function createNote(str) {
   let note = document.createElement("div");
-  note.textContent = str;
-  note.contentEditable = true;
+  note.className = "note";
+  let noteText = document.createElement("textarea");
+  noteText.value = str;
+  noteText.className = "note-text";
+  let close = createCloseButton();
+  note.append(close, noteText);
   container.append(note);
   console.log(container.children);
 }
 
+function createCloseButton() {
+  let button = document.createElement("button");
+  button.className = "close-button";
+  button.addEventListener("click", function () {
+    this.parentElement.remove();
+  });
+  return button;
+}
+
 function htmlToJSON() {
   let elementArray = [];
-  for (var element of container.children) {
-    elementArray.push(element.textContent);
+  for (var element of container.querySelectorAll("textarea")) {
+    elementArray.push(element.value);
   }
   return elementArray;
 }
@@ -32,5 +45,3 @@ function fetchToServer() {
     body: JSON.stringify(htmlToJSON()),
   });
 }
-
-// export { createNote };
