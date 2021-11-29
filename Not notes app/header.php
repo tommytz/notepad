5b4afa5e805f86b969068364f4670809ef286f4e
tommytz <ttardizuch@gmail.com>
2021-11-29 14:18:39 +1300

    Removed old files

--- Not notes app/header.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-<title>How to put PHP in HTML</title>
-</head>
-<body>
-  <script>alert("oops")</script>
-<div>This is pure HTML message.</div>
-<div>Next, we’ll display today’s date and day using PHP!</div>
--- notes/generateNotes.js
+++
@@ -1,7 +0,0 @@
-fetch("note_data.json").then(async function (response) {
-  let data = await response.json();
-
-  for (var str of data) {
-    createNote(str);
-  }
-});
